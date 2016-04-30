46c5937d5566d889a261a178b6d119db14b372fc
Zdeněk Gold <zigisps@gmail.com>
2016-04-30 20:38:31 +0200

    Diplomka

---
+++ JEvolution/src/org/evolution/util/TreeParam.java
@@ -0,0 +1,15 @@
+package org.evolution.util;
+
+public class TreeParam {
+	public int index = 0;
+	public int deep = -1;
+	public int maxDepth = 0;
+	public int count;
+
+	public void initialize() {
+		index = 0;
+		deep = -1;
+		maxDepth = 0;
+		count = 0;
+	}
+}
